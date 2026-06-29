67bd537a231908d39cb35370e51af0387258d736
MKoussa <matthew.koussa@gmail.com>
2026-06-29 10:52:10 -0700

    init crude scryfall api call

--- Program.fs
+++ Program.fs
@@ -1,2 +1,30 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open System.Net.Http
+open System.Net.Http.Json
+
+printfn "Starting Battle!"
+
+type magicCard = {
+    name: string
+}
+
+let client = new HttpClient()
+
+client.DefaultRequestHeaders.Add(name = "User-Agent", value = "ConsolePokeVsMTG/1.0")
+client.DefaultRequestHeaders.Add(name = "Accept", value = "application/json")
+
+let scryfallURL = "https://api.scryfall.com/cards/random"
+
+let main =
+    task {
+        try 
+            let! responseBody = client.GetFromJsonAsync<magicCard>(requestUri = scryfallURL)
+
+            printfn $"{responseBody.name}"
+        with
+        | :? HttpRequestException as (e: HttpRequestException) ->
+            printfn "\nException Caught!"
+            printfn $"Message :{e.Message}"
+    }
+
+main.Wait()
